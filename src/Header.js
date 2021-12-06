5cd84fdf6b7632b095c195342a413c7b941d3a7c
Jeffrey Platt <jsplatt@gmail.com>
2021-12-06 13:51:36 -0500

    just starting

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            Logo
+            About
+            Services
+            Contact
+        </div>
+    )
+}
+
+export default Header
