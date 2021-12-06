376835664e9bc80d28b3e10a7c2907f77f29c314
Jeffrey Platt <jsplatt@gmail.com>
2021-12-06 14:03:00 -0500

    figuring it out

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            Logo
+        <div className="header">
+            <img 
+                src="https://media.istockphoto.com/vectors/cannabis-icon-black-minimalist-icon-isolated-on-white-background-vector-id855073770?k=20&m=855073770&s=612x612&w=0&h=KRUcGJ5RjDp5T6Luc6k8n-aXoXr8AaziaxWdjnAZcrU="
+                className="header__image"    
+            />
             About
             Services
             Contact
